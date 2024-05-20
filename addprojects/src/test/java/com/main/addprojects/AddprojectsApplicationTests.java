aa0fcfaf4554af54f6fe66d0fbe5e6387335e3c0
t4tomer <tomer.polsky27@gmail.com>
2024-05-20 12:08:00 +0300

    open new Spring Project for adding products

---
+++ addprojects/src/test/java/com/main/addprojects/AddprojectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.addprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddprojectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
