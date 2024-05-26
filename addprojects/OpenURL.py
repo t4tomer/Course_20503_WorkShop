6c820a797e3b18245202735c442f943dac41333b
t4tomer <tomer.polsky27@gmail.com>
2024-05-26 21:25:32 +0300

    update

--- addprojects/OpenURL.py
+++ addprojects/OpenURL.py
@@ -3,11 +3,11 @@ import webbrowser
 # ! part of the original code
 url_RegisterLogin = "http://127.0.0.1:2023"
 #opents url of the book store 
-url_uploadImage="http://localhost:2023/add" # the orginal url of:spring-boot-mysql-image-upload
+url_addProduct="http://localhost:2023/add" # the orginal url of:spring-boot-mysql-image-upload
 
 # url1 = "http://127.0.0.1:8081/addProdcut.html"
 
 # webbrowser.open(url1)
 # webbrowser.open(url_uploadImage)
-webbrowser.open(url_RegisterLogin)
+webbrowser.open(url_addProduct)
 
